30042ceeeb434125458a76519f1064fe70d1bb44
lee ivan <leeivan2008@gmail.com>
2012-08-05 10:07:53 +0800

    update chapter10 for Location and map feature

---
+++ mcommerce-sample-android/src/com/google/mcommerce/sample/android/chapter10/BaiduMapActivity.java
@@ -0,0 +1,66 @@
+package com.google.mcommerce.sample.android.chapter10;
+
+import android.os.Bundle;
+
+import com.baidu.mapapi.BMapManager;
+import com.baidu.mapapi.GeoPoint;
+import com.baidu.mapapi.MapActivity;
+import com.baidu.mapapi.MapController;
+import com.baidu.mapapi.MapView;
+import com.google.mcommerce.sample.android.R;
+
+public class BaiduMapActivity extends MapActivity {
+
+	private BMapManager mBMapMan;
+
+	@Override
+	protected void onCreate(Bundle arg0) {
+		// TODO Auto-generated method stub
+		super.onCreate(arg0);
+		setContentView(R.layout.c10_map_baidu_layout);
+		mBMapMan = new BMapManager(getApplication());
+		mBMapMan.init("98795B8ACBE49CB19819B5EE900D3A3A55CD3EFA", null);
+		super.initMapActivity(mBMapMan);
+
+		MapView mMapView = (MapView) findViewById(R.id.bmapsView);
+		mMapView.setBuiltInZoomControls(true); // 设置启用内置的缩放控件
+
+		MapController mMapController = mMapView.getController(); // 得到mMapView的控制权,可以用它控制和驱动平移和缩放
+		GeoPoint point = new GeoPoint((int) (39.915 * 1E6),
+				(int) (116.404 * 1E6)); // 用给定的经纬度构造一个GeoPoint，单位是微度 (度 * 1E6)
+		mMapController.setCenter(point); // 设置地图中心点
+		mMapController.setZoom(12); // 设置地图zoom级别
+	}
+
+	@Override
+	protected void onDestroy() {
+		if (mBMapMan != null) {
+			mBMapMan.destroy();
+			mBMapMan = null;
+		}
+		super.onDestroy();
+	}
+
+	@Override
+	protected void onPause() {
+		if (mBMapMan != null) {
+			mBMapMan.stop();
+		}
+		super.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		if (mBMapMan != null) {
+			mBMapMan.start();
+		}
+		super.onResume();
+	}
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}

b91ff211944f8e25d7bce6404c120ae32d9d029d
lee ivan <leeivan2008@gmail.com>
2012-08-21 20:53:10 +0800

    json parse

--- mcommerce-sample-android/src/com/google/mcommerce/sample/android/chapter03/ParseJSONActivity.java
+++ mcommerce-sample-android/src/com/google/mcommerce/sample/android/chapter03/ParseJSONActivity.java
@@ -11,47 +11,45 @@ import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.google.mcommerce.sample.android.R;
 
 public class ParseJSONActivity extends Activity {
 
 	/** Called when the activity is first created. */
+	private static final String TAG = "ParseJSONActivity";
+	private TextView textViewCity;
+	private TextView textViewNick;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		String readTwitterFeed = readTwitterFeed();
-		try {
-			JSONArray jsonArray = new JSONArray(readTwitterFeed);
-			Log.i(ParseJSONActivity.class.getName(), "Number of entries "
-					+ jsonArray.length());
-			for (int i = 0; i < jsonArray.length(); i++) {
-				JSONObject jsonObject = jsonArray.getJSONObject(i);
-				Log.i(ParseJSONActivity.class.getName(),
-						jsonObject.getString("text"));
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		setContentView(R.layout.c03_parse_json_xml_layout);
+		textViewCity = (TextView) findViewById(R.id.c03_textView1);
+		textViewNick = (TextView) findViewById(R.id.c03_textView4);
+		//返回单个用户
+		String userUrl = "http://gw.api.taobao.com/router/rest?sign=EF87CC42B707AFF1234FF8782113CDFB&timestamp=2012-08-13+20%3A50%3A28&v=2.0&app_key=12129701&method=taobao.user.get&partner_id=top-apitools&format=json&nick=lihaifeng555&fields=user_id,uid,nick,sex,buyer_credit,seller_credit,location,created,last_visit,birthday,type,status,alipay_no,alipay_account,alipay_account,email,consumer_protection,alipay_bind";
+		String userString = getUsers(userUrl);
+		//返回多个用户
+		parseJson(userString);
+		String usersUrl = "http://gw.api.taobao.com/router/rest?sign=5AAFF7B2157B05B975282B71820F683F&timestamp=2012-08-21+12%3A22%3A35&v=2.0&app_key=12129701&method=taobao.users.get&partner_id=top-apitools&format=xml&nicks=lihaifeng555,andyy_tan&fields=user_id,nick,sex,buyer_credit,seller_credit,location,created,last_visit";
+		String usersString = getUsers(usersUrl);
+		parseJsonMulti(usersString);
 	}
 
-	public String readTwitterFeed() {
+	public String getUsers(String userUrl) {
 		StringBuilder builder = new StringBuilder();
 		HttpClient client = new DefaultHttpClient();
-		// HttpGet httpGet = new HttpGet(
-		// "http://twitter.com/statuses/user_timeline/vogella.json");
-		HttpGet httpGet = new HttpGet(
-				"http://gw.api.taobao.com/router/rest?sign=9E66FFFDE58D484F56B22B03802AF48C&timestamp=2012-08-13+11%3A04%3A57&v=2.0&app_key=12380481&method=taobao.user.get&partner_id=top-apitools&format=json&nick=lihaifeng555&fields=user_id,uid,nick,sex,buyer_credit,seller_credit,location,created,last_visit,birthday,type,status,alipay_no,alipay_account,alipay_account,email,consumer_protection,alipay_bind");
+		HttpGet httpGet = new HttpGet(userUrl);
 		try {
 			HttpResponse response = client.execute(httpGet);
 			StatusLine statusLine = response.getStatusLine();
@@ -66,8 +64,7 @@ public class ParseJSONActivity extends Activity {
 					builder.append(line);
 				}
 			} else {
-				Log.e(ParseJSONActivity.class.toString(),
-						"Failed to download file");
+				Log.e(TAG, "Failed to download file");
 			}
 		} catch (ClientProtocolException e) {
 			e.printStackTrace();
@@ -76,4 +73,46 @@ public class ParseJSONActivity extends Activity {
 		}
 		return builder.toString();
 	}
+
+	// 普通Json数据解析
+
+	private void parseJson(String strResult) {
+		try {
+			// 获取用户对象
+			JSONObject jsonUserObj = new JSONObject(strResult)
+					.getJSONObject("user");
+			String name = jsonUserObj.getString("nick");
+			JSONObject jsonLocationObj = jsonUserObj.getJSONObject("Location");
+			String city = jsonLocationObj.getString("city");
+			textViewCity.setText(city);
+			textViewNick.setText(name);
+		} catch (JSONException e) {
+			System.out.println("Json parse error");
+			e.printStackTrace();
+		}
+	}
+
+	// 解析多个数据的Json
+
+	private void parseJsonMulti(String strResult) {
+
+		try {
+			JSONArray jsonObjs = new JSONObject(strResult)
+					.getJSONArray("users");
+			String s = "";
+			for (int i = 0; i < jsonObjs.length(); i++) {
+				JSONObject jsonObj = ((JSONObject) jsonObjs.opt(i))
+						.getJSONObject("user");
+				int id = jsonObj.getInt("id");
+				String name = jsonObj.getString("name");
+				String gender = jsonObj.getString("gender");
+				s += "ID号" + id + ", 姓名：" + name + ",性别：" + gender + "\n";
+
+			}
+		} catch (JSONException e) {
+			System.out.println("Jsons parse error !");
+			e.printStackTrace();
+		}
+	}
+
 }

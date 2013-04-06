4668f9f204e9019ede7d5d32a8ad31e8a34643b0
lee ivan <leeivan2008@gmail.com>
2013-04-06 13:15:27 +0800

    Signed-off-by: lee ivan <leeivan2008@gmail.com>

---
+++ mcommerce-sample-android/src/com/google/mcommerce/sample/android/chapter03/multiplescreen/DensityIndependence.java
@@ -0,0 +1,15 @@
+package com.google.mcommerce.sample.android.chapter03.multiplescreen;
+
+import com.google.mcommerce.sample.android.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DensityIndependence extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.c03_density_independence);
+	}
+}

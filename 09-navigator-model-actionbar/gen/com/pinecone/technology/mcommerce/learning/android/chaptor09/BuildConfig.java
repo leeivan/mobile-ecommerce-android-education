9ca427da180924339d5fd5d8674652bc94600756
leeivan <leeivan2008@gmail.com>
2013-08-09 15:52:00 -0700

    commit

---
+++ 09-navigator-model-actionbar/gen/com/pinecone/technology/mcommerce/learning/android/chaptor09/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.pinecone.technology.mcommerce.learning.android.chaptor09;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ 09-navigator-model-menu/gen/com/pinecode/technology/mcommerce/learning/android/chapter09/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.pinecode.technology.mcommerce.learning.android.chapter09;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}

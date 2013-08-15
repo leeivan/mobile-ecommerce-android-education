b03711e3737ed8d2412a2d22afb29263a46716d5
leeivan <leeivan2008@gmail.com>
2013-08-14 21:06:55 -0700

    add project07 test

---
+++ 07-data-storage-sqlite/src/com/pinecone/technology/mcommerce/learning/android/chapter07/sqlite/Student.java
@@ -0,0 +1,59 @@
+package com.pinecone.technology.mcommerce.learning.android.chapter07.sqlite;
+
+import android.content.Context;
+
+public class Student {
+
+	int _id;
+	String _name;
+	int _age;
+	int _dept;
+
+	public Student(String name, int age, int dept) {
+
+		this._name = name;
+		this._age = age;
+		this._dept = dept;
+	}
+
+	public Student(String name, int age) {
+		this._name = name;
+		this._age = age;
+	}
+
+	public int getID() {
+		return this._id;
+	}
+
+	public void SetID(int id) {
+		this._id = id;
+	}
+
+	public String getName() {
+		return this._name;
+	}
+
+	public int getAge() {
+		return this._age;
+	}
+
+	public void setName(String name) {
+		this._name = name;
+	}
+
+	public void setAge(int age) {
+		this._age = age;
+	}
+
+	public void setDept(int dept) {
+		this._dept = dept;
+	}
+
+	public String getDeptName(Context con, int dept) {
+		return new DatabaseHelper(con).getDept(dept);
+	}
+
+	public int getDept() {
+		return this._dept;
+	}
+}

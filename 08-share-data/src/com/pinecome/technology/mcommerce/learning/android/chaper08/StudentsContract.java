0ecdd65fa0497262ae04d6e36fa38864b15c9207
leeivan <leeivan2008@gmail.com>
2013-08-17 00:38:31 -0700

    init project 08

---
+++ 08-share-data/src/com/pinecome/technology/mcommerce/learning/android/chaper08/StudentsContract.java
@@ -0,0 +1,45 @@
+package com.pinecome.technology.mcommerce.learning.android.chaper08;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public class StudentsContract {
+	public static final String AUTHORITY = "com.pinecone.technology.studentprovider";
+
+	private StudentsContract() {
+	}
+
+	// inner class describing columns and their types
+	public static final class Student implements BaseColumns {
+		public static final Uri CONTENT_URI = Uri.parse("content://"
+				+ AUTHORITY + "/students");
+		// Expose a content URI for this provider. This URI will be used to
+		// access the Content Provider
+		// from within application components using a ContentResolver
+		public static final String CONTENT_TYPE = "vnd.android.cursor.dir/student";
+		public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/student";
+		/**
+		 * SQL table columns
+		 */
+		public static final String DEFAULT_STUDENT_SORT_ORDER = "DeptId";
+		public static final String NAME = "StdName";
+		public static final String AGE = "Age";
+		public static final String DEPT = "DeptId";
+
+	}
+
+	public static final class Department implements BaseColumns {
+		public static final Uri CONTENT_URI = Uri.parse("content://"
+				+ AUTHORITY + "/departments");
+		// Expose a content URI for this provider. This URI will be used to
+		// access the Content Provider
+		// from within application components using a ContentResolver
+		public static final String CONTENT_TYPE = "vnd.android.cursor.dir/department";
+		public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/department";
+		/**
+		 * SQL table columns
+		 */
+		public static final String NAME = "DeptName";
+		public static final String DEFAULT_DEPARTMENT_SORT_ORDER = "DeptName";
+	}
+}
---
+++ 08-share-data/src/com/pinecome/technology/mcommerce/learning/android/chaper08/StudentsProvider.java
@@ -0,0 +1,205 @@
+package com.pinecome.technology.mcommerce.learning.android.chaper08;
+
+import java.util.HashMap;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.pinecone.technology.mcommerce.learning.android.chapter07.sqlite.DatabaseHelper;
+
+public class StudentsProvider extends ContentProvider {
+
+	private static final String STUDENT_TABLE = "students";
+	private static final String DEPARTMENT_TABLE = "departments";
+
+	private static final int STUDENT = 1;
+	private static final int STUDENT_ID = 2;
+	private static final int DEPARTMENT = 3;
+	private static final int DEPARTMENT_ID = 4;
+	private static final UriMatcher MATCHER;
+	private DatabaseHelper dbHelper = null;
+
+	static {
+		MATCHER = new UriMatcher(UriMatcher.NO_MATCH);
+		MATCHER.addURI(StudentsContract.AUTHORITY, "student", STUDENT);
+		MATCHER.addURI(StudentsContract.AUTHORITY, "student/#", STUDENT_ID);
+		MATCHER.addURI(StudentsContract.AUTHORITY, "department", DEPARTMENT);
+		MATCHER.addURI(StudentsContract.AUTHORITY, "department/#",
+				DEPARTMENT_ID);
+	}
+	private static HashMap<String, String> sStudentProjectionMap;
+	static {
+		sStudentProjectionMap = new HashMap<String, String>();
+		sStudentProjectionMap.put(StudentsContract.Student._ID,
+				StudentsContract.Student._ID);
+
+		sStudentProjectionMap.put(StudentsContract.Student.NAME,
+				StudentsContract.Student.NAME);
+		sStudentProjectionMap.put(StudentsContract.Student.AGE,
+				StudentsContract.Student.AGE);
+		sStudentProjectionMap.put(StudentsContract.Department.NAME,
+				StudentsContract.Department.NAME);
+	}
+
+	@Override
+	public boolean onCreate() {
+		Log.d("Provider", "onCreate");
+		dbHelper = new DatabaseHelper(getContext());
+		return ((dbHelper == null) ? false : true);
+	}
+
+	/**
+	 * Return the MIME type of the data at the given URI. This should start with
+	 * "vnd.android.cursor.item" for a single record, or
+	 * "vnd.android.cursor.dir" for multiple items. This method can be called
+	 * from multiple threads, as described in
+	 */
+	@Override
+	public String getType(Uri url) {
+		final int match = MATCHER.match(url);
+		switch (match) {
+		case STUDENT:
+			return StudentsContract.Student.CONTENT_TYPE;
+		case STUDENT_ID:
+			return StudentsContract.Student.CONTENT_ITEM_TYPE;
+		case DEPARTMENT:
+			return StudentsContract.Department.CONTENT_TYPE;
+		case DEPARTMENT_ID:
+			return StudentsContract.Department.CONTENT_ITEM_TYPE;
+		default:
+			throw new IllegalArgumentException("Unsupported URI: " + url);
+		}
+	}
+
+	@Override
+	public Cursor query(Uri url, String[] projection, String selection,
+			String[] selectionArgs, String sort) {
+		Log.d("Provider", "query");
+		SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+		Cursor c = null;
+		String orderBy = null;
+		switch (MATCHER.match(url)) {
+		case STUDENT:
+			qb.setTables(STUDENT_TABLE);
+			qb.setProjectionMap(sStudentProjectionMap);
+			if (TextUtils.isEmpty(sort)) {
+				orderBy = StudentsContract.Student.DEFAULT_STUDENT_SORT_ORDER;
+			} else {
+				orderBy = sort;
+			}
+			break;
+		case STUDENT_ID:
+			qb.setTables(STUDENT_TABLE);
+			qb.setProjectionMap(sStudentProjectionMap);
+			qb.appendWhere(StudentsContract.Student._ID + "="
+					+ url.getPathSegments().get(1));
+			break;
+		case DEPARTMENT:
+			qb.setTables(DEPARTMENT_TABLE);
+			qb.setProjectionMap(sStudentProjectionMap);
+			if (TextUtils.isEmpty(sort)) {
+				orderBy = StudentsContract.Department.DEFAULT_DEPARTMENT_SORT_ORDER;
+			} else {
+				orderBy = sort;
+			}
+
+			break;
+		case DEPARTMENT_ID:
+			qb.setTables(DEPARTMENT_TABLE);
+			qb.setProjectionMap(sStudentProjectionMap);
+			qb.appendWhere(StudentsContract.Department._ID + "="
+					+ url.getPathSegments().get(1));
+			break;
+		}
+		c = qb.query(dbHelper.getReadableDatabase(), projection, selection,
+				selectionArgs, null, null, orderBy);
+		c.setNotificationUri(getContext().getContentResolver(), url);
+		return (c);
+	}
+
+	@Override
+	public Uri insert(Uri url, ContentValues initialValues) {
+		if (MATCHER.match(url) != STUDENT) {
+			throw new IllegalArgumentException("Unknown URI " + url);
+		}
+		if (initialValues.containsKey(StudentsContract.Student.NAME) == false) {
+			throw new SQLException(
+					"Failed to insert row because Book Name is needed " + url);
+		}
+		long rowID = dbHelper.getWritableDatabase().insert(STUDENT_TABLE,
+				StudentsContract.Student.NAME, initialValues);
+		if (rowID > 0) {
+			Uri uri = ContentUris.withAppendedId(
+					StudentsContract.Student.CONTENT_URI, rowID);
+			getContext().getContentResolver().notifyChange(uri, null);
+			return (uri);
+		}
+		throw new SQLException("Failed to insert row into " + url);
+	}
+
+	@Override
+	public int delete(Uri url, String where, String[] whereArgs) {
+		SQLiteDatabase db = dbHelper.getWritableDatabase();
+		int count;
+		switch (MATCHER.match(url)) {
+		case STUDENT:
+			count = db.delete(STUDENT_TABLE, where, whereArgs);
+			break;
+		case STUDENT_ID:
+			String rowId = url.getPathSegments().get(1);
+			count = db.delete(
+					STUDENT_TABLE,
+					StudentsContract.Student._ID
+							+ "="
+							+ rowId
+							+ (!TextUtils.isEmpty(where) ? " AND (" + where
+									+ ')' : ""), whereArgs);
+			break;
+		default:
+			throw new IllegalArgumentException("Unknown URI " + url);
+		}
+		getContext().getContentResolver().notifyChange(url, null);
+		return (count);
+	}
+
+	@Override
+	public int update(Uri url, ContentValues values, String where,
+			String[] whereArgs) {
+		SQLiteDatabase db = dbHelper.getWritableDatabase();
+		int count;
+		switch (MATCHER.match(url)) {
+		case STUDENT:
+			count = db.update(STUDENT_TABLE, values, where, whereArgs);
+			break;
+
+		case STUDENT_ID:
+			String rowId = url.getPathSegments().get(1);
+			count = db.update(
+					STUDENT_TABLE,
+					values,
+					StudentsContract.Student._ID
+							+ "="
+							+ rowId
+							+ (!TextUtils.isEmpty(where) ? " AND (" + where
+									+ ')' : ""), whereArgs);
+			break;
+
+		default:
+			throw new IllegalArgumentException("Unknown URI " + url);
+		}
+		count = dbHelper.getWritableDatabase().update(STUDENT_TABLE, values,
+				where, whereArgs);
+		getContext().getContentResolver().notifyChange(url, null);
+		return (count);
+	}
+
+}

498cbb5e017c4648066e9e029c9f264170572961
leeivan <leeivan2008@gmail.com>
2013-09-07 21:27:19 -0700

    update

--- 10-navigator-model/gen/com/pinecone/technology/mcommerce/learning/android/chaptor09/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.pinecone.technology.mcommerce.learning.android.chaptor09;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
